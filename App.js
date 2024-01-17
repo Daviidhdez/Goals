63ed156b59dba150bddc28edaf9117e8a5b97b65
Daviidhdez <davidhernanmoreno56@gmail.com>
2024-01-17 11:40:45 +0100

    2º Parte-Funcionalidad Hooks estados

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  //Declaramos el hook de estado de componente "newGoal"
+  const [newGoal, setNewGoal] = useState("");
+
+  const [myGoals, setMyGoals] = useState([]);
+
+  function textChangeHandler(enteredText) {
+    setNewGoal(enteredText);
+  }
+
+  function addGoalHandler() {
+    setMyGoals(myCurrentGoals => [...myCurrentGoals, newGoal])
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Input your Goal' />
-        <Button title='Add Goal' />
+        <TextInput onChangeText={textChangeHandler} style={styles.textInput} placeholder='Input your Goal' />
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>Your List of goals...</Text>
+        {myGoals.map((goal, i) => {
+          return (
+            <View key={i}>
+              <Text>{goal}</Text>
+            </View>
+          )
+        })}
+
       </View>
     </View>
   );
@@ -19,6 +41,7 @@ const styles = new StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 70,
+    backgroundColor: "#D5F8F3",
     paddingHorizontal: 15
   },
 
@@ -28,15 +51,18 @@ const styles = new StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: 20,
     borderBottomWidth: 1,
-    borderBottomColor: "#CCCC",
+    borderBottomColor: "#076C4C",
     paddingBottom: 20,
     alignItems: "center"
 
   },
 
   textInput: {
-    borderColor: "#CCCC",
+    borderColor: "green",
+    backgroundColor: "#ACD176",
     borderWidth: 1,
+    fontWeight: "bold",
+    fontSize: 17,
     width: "70%",
     padding: 10
   },
